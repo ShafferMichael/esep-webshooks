c9a5996cc38028814b7ea4d4d30877eecbfb0176
Michael Shaffer <michaelshaffer45@gmail.com>
2025-04-11 20:23:05 -0400

    updated default

--- EsepWebhook/src/EsepWebhook/obj/Release/net8.0/linux-x64/EsepWebhook.AssemblyInfo.cs
+++ EsepWebhook/src/EsepWebhook/obj/Release/net8.0/linux-x64/EsepWebhook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38c0e7f6b9ef8d954820832858c9314b9fc975f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+056a40f7d9b1b00ea9e6a13375c9260b4d918661")]
 [assembly: System.Reflection.AssemblyProductAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
